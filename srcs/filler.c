7ab1fd148089314b7ecbcb09e52cea30e0379374
Mathieu Jouffroy <mathieu.jouffroy@gmail.com>
2019-05-08 13:49:05 +0200

    filler main functions

---
+++ srcs/filler.c
@@ -0,0 +1,40 @@
+#include "filler.h"
+
+int		init_game(t_filler *filler)
+{
+	if (!(get_player_id(filler)))
+		return (errors(filler, 0));
+	if (!get_map_size(filler))
+		return (errors(filler, 1));
+	if (!init_map(filler))
+		return (0);
+	filler->size = filler->map.width * filler->map.height;
+	filler->pnt.min = filler->size;
+	return (1);
+}
+
+int		start_game(t_filler *filler)
+{
+	int		playing;
+
+	playing = 1;
+	while (playing)
+	{
+		if (!parse_map(filler))
+			break ;
+		if (!parse_piece(filler))
+			break ;
+		playing = play(filler);
+	}
+	return (1);
+}
+
+int main(void)
+{
+	t_filler filler;
+
+	ft_bzero(&filler, sizeof(filler));
+	init_game(&filler);
+	start_game(&filler);
+	return (0);	
+}
