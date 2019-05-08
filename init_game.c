a059f81023ef785f5505abb51963703af68f06f5
Mathieu Jouffroy <mathieu.jouffroy@gmail.com>
2019-05-08 13:45:54 +0200

    init game done, get info from vm + init map and piece

---
+++ init_game.c
@@ -0,0 +1,109 @@
+#include "filler.h"
+
+int	get_player_id(t_filler *filler)
+{
+	char		*line;
+	int			i;
+
+	line = NULL;
+	i = 0;
+	if (get_next_line(0, &line) <= 0)
+	{
+		ft_memdel((void**)&line);
+		return (0);
+	}
+	if ((ft_strncmp(line, "$$$ exec p", 10)) && (line[12] != ':'))
+		return (0);
+	if (line[14] == '[')
+		while(line[14 + i])
+			i++;
+	else
+		return (0);
+	if (line[12] != ':' || line[13 + i] != ']')
+		return (0);
+	if (line[10] == '1' || line[10] == '2')
+	{
+		filler->me = (line[10] == '1') ? 'O' : 'X';
+		filler->bot = (line[10] == '1') ? 'X' : 'O';
+	}
+	else
+		return (0);
+	ft_memdel((void**)&line);
+	return (1);
+}
+
+int  get_map_size(t_filler *filler)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	line = NULL;
+	if (get_next_line(0, &line) <= 0)
+	{
+		ft_memdel((void**)&line);
+		return (0);
+	}
+	filler->data = ft_strdup(line);
+	if (ft_strncmp(line, "Plateau ", 8)
+		|| !(filler->map.height = ft_atoi(line + 8)))
+		return (0);
+	i = ft_digitcount(filler->map.height) + 1;
+	if (!(filler->map.width = ft_atoi(line + 8 + i)))
+		return (0);
+	i += ft_digitcount(filler->map.width);
+	if (line[8 + i] != ':')
+		return (0);
+	ft_memdel((void**)&line);
+	return (1);
+}
+
+int	get_piece_size(t_filler *filler)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	line = NULL;
+	if (get_next_line(0, &line) <= 0)
+		return (0);
+	if (ft_strncmp(line, "Piece ", 6)
+		|| (!(filler->piece.height = ft_atoi(line + 6))))
+		return (0);
+	i = ft_digitcount(filler->piece.height) + 1;
+	if (!(filler->piece.width = ft_atoi(line + 6 + i)))
+		return (0);
+	i += ft_digitcount(filler->piece.width);
+	if (line[6 + i] != ':')
+		return (0);
+	ft_memdel((void**)&line);
+	return (1);
+}
+
+int  init_map(t_filler *filler)
+{
+    int i;
+
+    filler->map.board = (char**)ft_memalloc(sizeof(char*) * filler->map.height);
+    i = 0;
+    while (i < filler->map.height)
+    {
+        filler->map.board[i] = (char*)ft_memalloc(sizeof(char) * filler->map.width + 1);
+        i++;
+    }
+    return (1);
+}
+
+int		init_piece(t_filler *filler)
+{
+	int i;
+
+	filler->piece.square = (char**)ft_memalloc(sizeof(char*) * filler->piece.height);
+	i = 0;
+	while (i < filler->piece.height)
+	{
+		filler->piece.square[i] = (char*)ft_memalloc(sizeof(char) * filler->piece.width + 1);
+		i++;
+	}
+	return (1);
+}
